b61ae11aefec85ef85646c44d3d4cb6d998dfc10
Prachi <raj.prachi290@gmail>
2022-02-11 13:00:38 +0530

    first commit

---
+++ reverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i=1,m=1;
+	printf("enter no\n");
+	scanf("\n%d",&n);
+	while(n+1>i)
+	 { printf("\n%d",i);
+	 i++;}
+	 printf("\nreverse");
+	 while(n>0)
+	 { printf("\n%d",n);
+	 n--;}
+
+    return 0;
+}
